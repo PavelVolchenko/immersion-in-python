a541a9f5ce91abb92678aa00817bf65f21090691
Pavel Volchenko <2793553@gmail.com>
2023-08-15 19:29:21 +0300

    hw09 done

--- hw09/task01.py
+++ hw09/task01.py
@@ -3,32 +3,24 @@
     - Генерация csv файла с тремя случайными числами в каждой строке. 100-1000 строк.
     - Декоратор, запускающий функцию нахождения корней квадратного уравнения с каждой тройкой чисел из csv файла.
     - Декоратор, сохраняющий переданные параметры и результаты работы функции в json файл.    """
+
 import csv
+import json
 from random import randint
-from typing import Callable
 
 
-def from_csv(func: Callable):
-    def wrapper(*args):
-        result = list()
-        with open('numbers.csv', newline='') as file:
-            reader = csv.reader(file, quoting=csv.QUOTE_NONE)
-            for row in reader:
-                a, b, c = map(int, str(*row).split(' '))
-                result.append(func(a, b, c))
-            return result
-    return wrapper
+def from_csv():
+    result = list()
+    with open('numbers.csv', newline='') as file:
+        reader = csv.reader(file, quoting=csv.QUOTE_NONE)
+        for row in reader:
+            result.append(str(*row))
+        return result
 
-# def safe(func: Callable):
-#     def wrapper(*args):
-#         result = list()
-#         with open('numbers.csv', newline='') as file:
-#             reader = csv.reader(file, quoting=csv.QUOTE_NONE)
-#             for row in reader:
-#                 a, b, c = map(int, str(*row).split(' '))
-#                 result.append(func(a, b, c))
-#             return result
-#     return wrapper
+
+def json_writer(result):
+    with open('result.json', 'w', encoding='utf-8') as file:
+        json.dump(result, file, indent=2, ensure_ascii=False)
 
 
 def generate_csv(rows: int = 100) -> str:
@@ -39,43 +31,40 @@ def generate_csv(rows: int = 100) -> str:
     return f"Файл csv успешно сгенерирован.\nКол-во строк: {rows}"
 
 
-@safe
-@from_csv
-def find_roots(a, b, c: int):
-    # print(type(numbers))
-    # print(numbers)
-    # a, b, c = map(int, numbers.split(' '))
+def deco_roots(func):
+    abc_list = from_csv()
 
+    def inner():
+        result = dict()
+        for abc in abc_list:
+            a, b, c = map(int, abc.split(' '))
+            result[f'{a=}, {b=}, {c=}'] = func(a, b, c)
+        return result
+
+    return inner
+
+
+def deco_json_writer(func):
+
+    def inner():
+        result = func()
+        json_writer(result)
+        return result
+
+    return inner
+
+
+@deco_json_writer
+@deco_roots
+def find_roots(a, b, c: int):
     d = b ** 2 - 4 * a * c
     if d < 0:
         return "Корней нет"
     elif d == 0:
         return f'x: {-b / (2 * a):.2f}'
     elif d > 0:
-        # x1 = (-b + d ** 0.5) / (2 * a)
-        # x2 = (-b - d ** 0.5) / (2 * a)
         return f'x1: {(-b + d ** 0.5) / (2 * a):.2f}, x2: {(-b - d ** 0.5) / (2 * a):.2f}'
 
 
-# def find_roots(numbers=None):
-#     a, b, c = map(int, f'{randint(-100, 100)} {randint(-100, 100)} {randint(-100, 100)}'.split(' '))
-#     d = b ** 2 - 4 * a * c
-#     print(f"a = {a}, b = {b}, c = {c}")
-#     print("Корней нет" if d < 0 else
-#           "Есть один корень" if d == 0 else
-#           "Есть два различных корня")
-#     if d == 0:
-#         x = -b / (2 * a)
-#         print(x)
-#     elif d > 0:
-#         x1 = (-b + d ** 0.5) / (2 * a)
-#         x2 = (-b - d ** 0.5) / (2 * a)
-#         print(f"x1 = {x1:.2f}, x2 = {x2:.2f}")
-
-generate_csv(10)
-# r = find_roots()
-# print(r)
-# print(generate_csv(10))
-
-# print(f := from_csv("10 100 100"))
-# print(f)
+generate_csv(100)
+print(find_roots())
